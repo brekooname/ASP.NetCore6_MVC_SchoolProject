673edbdc4fd853db1c7c21ea9d2cec01f1ce9c38
onrdrmn <onrdrmn@gmail.com>
2022-10-08 01:25:34 +0300

    Service and Manager Abstractions & Concretions Integrated

---
+++ Business/Concrete/ExamGradeManager.cs
@@ -0,0 +1,11 @@
+﻿
+using Business.Abstract;
+using DataAccess.Repository.Abstract;
+
+namespace Business.Concrete
+{
+    public class ExamGradeManager : IExamGradeService
+    {
+        IExamGradeRepository _examGradeRepository;
+    }
+}
